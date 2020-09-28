9f12f12c2d0aee597a491fc65116c5eb34c852b2
GuoCheng12 <71240524+GuoCheng12@users.noreply.github.com>
2020-09-28 19:59:32 +0800

    Add files via upload

---
+++ 20080123_Assign1_x.c
@@ -0,0 +1,22 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//#include <math.h>
+//
+//
+//
+//int main()
+//{
+//	double a,b,c,p,s;
+//	printf("请输入三边\n");
+//	scanf ("%lf%lf%lf",&a,&b,&c);
+//	p=(a+b+c)/2;
+//	if(a+b>c && a+c>b && b+c>a) //判断是否可以构成三角形。
+//	{
+//	s=sqrt(p*(p-a)*(p-b)*(p-c));
+//		printf("面积为%lf\n",s);
+//		printf("周长为%lf\n",p*2);
+//	}
+//	else
+//		printf("无法构成三角形\n");
+//	return 0;
+//}
---
+++ 20080123_Assign2_x.c
@@ -0,0 +1,19 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//#define PI 3.1415
+//int main ()
+//{
+//	double r,s,zc;
+//	printf("请输入半径\n");
+//	scanf("%lf",&r);
+//	
+//		s = PI*r*r;
+//        zc = PI*2*r;
+//
+//
+//	printf("面积为:%f\n",s);
+//	printf("周长为:%f\n",zc);
+//
+//
+//	return 0;
+//}
